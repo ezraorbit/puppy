4933c22801de7afe6a9a4a02384053663d1a9821
ezraorbit <emakankwasa@alu.ucam.edu>
2024-04-03 12:51:12 +0200

    add WhenGrownUps dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ezraorbit/puppy
 
 go 1.22.0
+
+require github.com/ezraorbit/dog v0.0.0-20240403104548-8abb33ab02f1
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/ezraorbit/dog"
+
 func Bark() string {
 	return "Yah boy!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Okay Bark! Bark!"
+}
+
+func grownUp() string {
+	return "I'm all grown up! BAAAAAAARK!!!!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUps(grownUp())
 }
